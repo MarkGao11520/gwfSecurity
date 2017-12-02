375b9a4ff99ff5d495163eb5c725bc9b84a39f48
MarkGao11520 <1152057576@qq.com>
2017-12-02 18:14:12 +0800

    session管理

--- spring-boot-api-project-seed/src/main/java/com/gwf/dao/BBlogMapper.java
+++
@@ -1,7 +0,0 @@
-package com.gwf.dao;
-
-import com.gwf.core.Mapper;
-import com.gwf.model.BBlog;
-
-public interface BBlogMapper extends Mapper<BBlog> {
-}
--- spring-boot-api-project-seed/src/main/java/com/gwf/dao/BLabelMapper.java
+++
@@ -1,7 +0,0 @@
-package com.gwf.dao;
-
-import com.gwf.core.Mapper;
-import com.gwf.model.BLabel;
-
-public interface BLabelMapper extends Mapper<BLabel> {
-}
